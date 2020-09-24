a29345cf7d132a02902aa7cef6b5a775a0ff5143
johmara <johan.t.martinson@gmail.com>
2020-09-24 23:41:15 +0200

    Home page created

---
+++ src/components/Home.vue
@@ -0,0 +1,51 @@
+<template>
+    <body>
+        <main>
+            <header>
+                <nav ref="NavBar">
+                    <img class="logo" src="../assets/johmara.svg" alt="Johan Martinson">
+                    <div class="links">
+                        <a href="#" style="color: #202531">About</a>
+                        <a href="#" style="color: #202531">Projects</a>
+                    </div>
+                </nav>
+            </header>
+            <section class="information">
+                <h1 class="hero_font">Johan Martinson</h1>
+                <h3 class="sub_header"> Project Manager | Developer | .....</h3>
+                    <div class="icons">
+                        <div class="icon">
+                                <img src="../assets/EmailIcon.svg" alt="Email" href="mailto:johan.t.martinson@gmail.com">
+                                <div class="icon_info">
+                                    <a class="icon_top" href="mailto:johan.t.martinson@gmail.com" style="color: #202531">Email</a>
+                                </div>
+                            </div>
+                            <div class="icon">
+                                <img src="../assets/GithubIcon.svg" alt="Email" href="https://github.com/johmara/">
+                                <div class="icon_info">
+                                    <a class="icon_top" href="https://github.com/johmara/" style="color: #202531">Github</a>
+                                </div>
+                            </div>
+                            <div class="icon">
+                                <img src="../assets/LinkedInIcon.svg" alt="Email" href="https://www.linkedin.com/in/johanmartinson/">
+                                <div class="icon_info">
+                                    <a class="icon_top" href="https://www.linkedin.com/in/johanmartinson/" style="color: #202531">LinkedIn</a>
+                                </div>
+                            </div>
+                    </div>
+            </section>
+
+
+        </main>
+    </body>
+</template>
+
+<script>
+export default {
+    name: 'Home'
+}
+</script>
+
+<style>
+
+</style>
