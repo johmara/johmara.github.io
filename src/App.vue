7a5372f7c4a45eea36ded4f3221f21f6ec27473c
johmara <johan.t.martinson@gmail.com>
2020-10-06 23:24:00 +0200

    prepare for vuetify home

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
   <v-app>
+    <!--
+      <NavBar/>
+    -->
     <v-main>
+      <!--
+      <v-container fluid>
+        <router-view/>
+      </v-container>
+      -->
       <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
+/*
+import NavBar from './components/Navbar'
 
+export default {
+    name: 'app',
+
+    components:{
+        NavBar,
+    },
+
+    data: () => ({
+        
+    }),
+};
+*/
 </script>
