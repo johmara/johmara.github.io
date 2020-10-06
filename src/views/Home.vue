626a37f1e6ebf090ca968acf93ec1260b81e7ee6
johmara <johan.t.martinson@gmail.com>
2020-10-06 23:25:04 +0200

    Fixed linking to icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,26 +7,32 @@
     <section class="information">
         <h1 class="hero_font">Johan Martinson</h1>
         <h3 class="sub_header"> Project Manager | Developer </h3>
-            <div class="icons">
-                <div class="icon">
-                        <img src="../assets/EmailIcon.svg" alt="Email" href="mailto:johan.t.martinson@gmail.com">
-                        <div class="icon_info">
-                            <a class="icon_top" href="mailto:johan.t.martinson@gmail.com" style="color: #202531">Email</a>
-                        </div>
-                    </div>
-                    <div class="icon">
-                        <img src="../assets/GithubIcon.svg" alt="Email" href="https://github.com/johmara/">
-                        <div class="icon_info">
-                            <a class="icon_top" href="https://github.com/johmara/" style="color: #202531">Github</a>
-                        </div>
-                    </div>
-                    <div class="icon">
-                        <img src="../assets/LinkedInIcon.svg" alt="Email" href="https://www.linkedin.com/in/johanmartinson/">
-                        <div class="icon_info">
-                            <a class="icon_top" href="https://www.linkedin.com/in/johanmartinson/" style="color: #202531">LinkedIn</a>
-                        </div>
-                    </div>
+        <div class="icons">
+            <div class="icon">
+                <a href="mailto:johan.t.martinson@gmail.com">
+                    <img src="../assets/EmailIcon.svg" alt="Email" class="ico">
+                </a>
+                <div class="icon_info">
+                    <a class="icon_top" href="mailto:johan.t.martinson@gmail.com" style="color: #202531">Email</a>
+                </div>
             </div>
+            <div class="icon">
+                <a href="https://github.com/johmara/">
+                    <img src="../assets/GithubIcon.svg" alt="Email" class="ico">
+                </a>
+                <div class="icon_info">
+                    <a class="icon_top" href="https://github.com/johmara/" style="color: #202531">Github</a>
+                </div>
+            </div>
+            <div class="icon">
+                <a href="https://www.linkedin.com/in/johanmartinson/">
+                    <img src="../assets/LinkedInIcon.svg" alt="Email" class="ico">
+                </a>
+                <div class="icon_info">
+                    <a class="icon_top" href="https://www.linkedin.com/in/johanmartinson/" style="color: #202531">LinkedIn</a>
+                </div>
+            </div>
+        </div>
     </section>
 </main>
 </body>
