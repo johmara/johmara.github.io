de8e739d79f7870c878943194ac98d3dd1813286
johmara <johan.t.martinson@gmail.com>
2020-10-14 00:16:50 +0200

    add Project definition

---
+++ src/components/LockAR/ProjectDefine.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-container fluid>
+        <v-row dense>
+            <v-col cols="12" md="6">
+                <v-card class="pa-6 ma-6" elevation="4">
+                    <h1 class="display-3">Research Questions</h1>
+                    
+                    <div class="py-10 my-5">
+                        <v-card-title>How can AR be used to authenticate access of a physical entity?</v-card-title>
+                        <v-card-title>How can we balance the convinience of AR with the security of AR authentication?</v-card-title>
+                    </div>
+                    <v-row class="mt-2">
+                        <v-col cols="4">
+                            <p class="text-right">Design Challenge: </p>
+                        </v-col>
+                        <v-col cols="8">
+                            <p class="font-weight-light">Authentication & access to physical entities</p>
+                        </v-col>
+                    </v-row>
+                    
+                    <v-row>
+                        <v-col cols="4">
+                            <p class="text-right">Approach:</p>
+                        </v-col>
+                        <v-col cols="8">
+                            <p class="font-weight-light">Research through Design</p>
+                        </v-col>
+                    </v-row>
+                </v-card> 
+            </v-col> 
+            <v-col cols="12" md="6">
+                <v-card class="pa-6 ma-6" elevation="4">
+                    <v-skeleton-loader type="card, card" loading>
+                        
+                    </v-skeleton-loader>
+                </v-card> 
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'ProjectDefine',
+
+};
+</script>
--- src/views/LockAR.vue
+++ src/views/LockAR.vue
@@ -7,41 +7,45 @@
         <v-container class="my-5">
             <v-row>
             <v-col align-self="center">
-                <v-img src="../assets/lockar.svg" min-height="70" max-height="380" contain center></v-img>           
+                <v-img src="../assets/lockar.svg" min-height="70" max-height="300" contain center></v-img>           
             </v-col>
             </v-row>
             <v-row> 
             <v-col cols="12">
                 <div class="introduction">
-                    <h1>LockAR</h1>
-                    <h3>An Augmented Reality Authentication System</h3> 
-                    <span class="caption">LockAR is a smart phone APP that provides virtual AR keys for users to pinpoint and unlock connected doors.</span> 
+                    <h1 class="display-2">LockAR</h1>
+                    <span class="subtitle-2">A smart phone app that provides virtual Augmented Reality keys for users to pinpoint and unlock connected doors.</span> 
                 </div>
             </v-col> 
             </v-row>
     </v-container>
     </v-parallax>
     <v-container>
-        <PB/>
+        <ProjectBrief/>
     </v-container>
     <v-container>
-        <ProjStruct/>
+        <ProjectStructure/>
+    </v-container>
+    <v-container>
+        <ProjectDefine/>
     </v-container>
 </div>
 </template>
 
 <script>
 import Navbar from '../components/Navbar'
-import PB from '../components/LockAR/ProjectBrief'
-import ProjStruct from '../components/LockAR/ProjectStructure'
+import ProjectBrief from '../components/LockAR/ProjectBrief'
+import ProjectStructure from '../components/LockAR/ProjectStructure'
+import ProjectDefine from '../components/LockAR/ProjectDefine'
 
 export default {
     name: 'LockAR',
 
     components:{
         Navbar,
-        PB,
-        ProjStruct
+        ProjectBrief,
+        ProjectStructure,
+        ProjectDefine,
     },
 
     data: () => ({
