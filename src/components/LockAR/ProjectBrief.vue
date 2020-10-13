3745e8d0daae17a0ac0cc93ef544c3c43aeefbd7
johmara <johan.t.martinson@gmail.com>
2020-10-14 00:28:29 +0200

    placeholder img

--- src/components/LockAR/ProjectBrief.vue
+++ src/components/LockAR/ProjectBrief.vue
@@ -1,16 +1,15 @@
 <template>
     <v-container>
-        <v-row>
-            <v-col md="4" offset-md="8" cols="6" offset="6">
-                <h1 class="text-right">Project Brief</h1>
-                
-            </v-col>
-        </v-row>
+        <h1 class="text-right display-4">Project Brief</h1>
         <v-row >
-            <v-col cols="12" md="6" offset-md="6" >
+            <v-col cols="12" md="6">
+                <v-skeleton-loader type="image"></v-skeleton-loader>
+            </v-col>
+            <v-col cols="12" md="6">
                 <p class="text-right"> Authentication has to do with proving one’s identity. Access has to do with permission to reach certain information, places, or a things. Even though the two are different concepts, in many applications they are closely related.</p>
                 <p class="text-right"> The theme is related to the Vetenskapsrådet-funded research project "Precious Keys," currently led by Mehmet Aydın Baytaş, Assoc. Prof. Ylva Fernaeus (KTH/Umeå), and PhD student Andreas Lindegren (KTH). Projects which might make contributions to the research may be invited to continue. Possibilities include thesis projects; for-credit project course; research internships at Chalmers, KTH, and Umeå; and industrial internships at our collaborators which include Assa Abloy, ManyOne, and Volvo Cars. </p>     
             </v-col>
+            
         </v-row>
     </v-container>
 </template>
