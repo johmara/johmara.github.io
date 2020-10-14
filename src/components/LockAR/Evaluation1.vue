03f31b5e5efa2a1b38f7285ae9d4a76eed3f4064
johmara <johan.t.martinson@gmail.com>
2020-10-14 10:48:32 +0200

    text added to eval1

--- src/components/LockAR/Evaluation1.vue
+++ src/components/LockAR/Evaluation1.vue
@@ -3,7 +3,23 @@
         <h1 class="display-3 text-center">Evaluation 1</h1>
             <v-row>
                 <v-col cols="12" md="6"> 
+                    <v-card flat class="pa-6 my-10">
+                        <v-card-title class="headline">Observation</v-card-title>
+                        <p class="pt-4">Participants try the prototypes to fullfill certain tasks.</p>
+                        <p class="pt-4"> Simple questions asked afterwards </p>
 
+                        <div class="py-5 my-5">
+                            <v-row>
+                                <v-col cols="4">
+                                    <p class="text-right">Participants:</p>
+                                </v-col>
+                                <v-col cols="8">
+                                    <p class="font-weight-light">5 participants age from 18-57</p>
+                                </v-col>
+                            </v-row>
+                            
+                        </div>
+                    </v-card>
                 </v-col>
                 <v-col cols="12" md="6">
                         <v-carousel hide-delimiters cycle progress progress-color="accent" 
