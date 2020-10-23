d90c181b8c40f6bd68c71fdd93fbd719f9feb8dd
johmara <johan.t.martinson@gmail.com>
2020-10-24 00:22:48 +0200

    Demo Video

--- src/components/LockAR/DemoVideo.vue
+++ src/components/LockAR/DemoVideo.vue
@@ -1,7 +1,23 @@
 <template>
-    <v-container>
+    <v-container > 
         <h1 class="display-2 text-center">Demo Video</h1>
+            <v-row>
+                <v-spacer></v-spacer>
+                <v-col cols="3">
+                    <!--TODO: fix center alignment-->
+                    <video height="500" controls class="justify-center">
 
+                    <source :src='require("@/assets/LockAR/impl/demo.mp4")' type="video/mp4">
+
+                    Your browser does not support the video tag.
+
+                    </video>
+                </v-col>
+                <v-spacer></v-spacer>
+            </v-row>
+                
+            
+                
         
 
         <p class="text-center">Due to time budget and ARcore’s special device requirements, instead of implementing a fully functioned APP, this peorototype is aimed for demostrating the core experience and conducting further evaluations.</p>
