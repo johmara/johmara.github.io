4006f1d929c09949ef1038bfcba5b5f1db4fc3ee
johmara <johan.t.martinson@gmail.com>
2021-01-22 17:35:04 +0100

    week 1 gaining domain knowledge

--- src/assets/HANS/Blog/post.js
+++ src/assets/HANS/Blog/post.js
@@ -2,8 +2,28 @@ module.exports = {
     data: () => (
         [
             {
+            date: '22-01-2021',
+            title: "Gaining domain knowledge",
+            subtitle: "First week down, many to go!",
+            image: "",
+            text: [
+                ` 
+                 During this week we have focused on finding what useful tools that we have available to us in IntelliJs
+                 plugin development and in FAXE. IntelliJ has a thorough guide that shows how to create a plugin which is 
+                 quite easy to follow and gives us a better understanding of how it works. IntelliJ's documentation have also
+                 proved to be useful for finding what version of Java (JDK-11) we need to use as well as dependency manager, code 
+                 conventions and recomended plugins for development. The FAXE tool contains everything we need for now. 
+                 As we learn more about it we might have new requirements regarding FAXE's endpoints. 
+                `,
+                `Next week we are rushing ahead to finish the tutorials on intellijs website and discuss the initial requirements of 
+                the tool. After the requirements have been set we can move on towards creating an object model. Adding to these goals 
+                reading and gaining better understanding of the available tools is also high on the list`,
+                `< Johan & Herman >`
+                ]
+            },
+            {
             date: '15-01-2021',
-            title: "Getting Started!",
+            title: "Getting started!",
             subtitle: "",
             image: "",
             text: [
