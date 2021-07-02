ee7dcbfd6315da82182a1ae2495d605e356d3794
johmara <johan.t.martinson@gmail.com>
2021-07-02 16:30:01 +0200

    added banner for email

--- src/views/Spexet.vue
+++ src/views/Spexet.vue
@@ -5,6 +5,8 @@
         <img src="../assets/spexet/inbjudan.png" alt="">
         <img src="../assets/spexet/instagram.png" alt="">
         <img src="../assets/spexet/logga_spex_transparent.png" alt="">
+        <img src="../assets/spexet/Banner-01.png" alt="">
+
     </v-container>
 </div>
 </template>
