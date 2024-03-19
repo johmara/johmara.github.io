946f66aabd92e6b4c14332314f00ff5f554e34ef
Johan Martinson <JohanM@centiro.se>
2024-03-19 14:18:26 +0100

    re added components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {SocialsComponent} from "../socials/socials.component";
+import {ProjectsComponent} from "../projects/projects.component";
+import {PublicationsComponent} from "../publications/publications.component";
+import { TimeLineComponent } from '../time-line/time-line.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [TimeLineComponent, SocialsComponent, ProjectsComponent, PublicationsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  title = 'johmara'
 
 }
