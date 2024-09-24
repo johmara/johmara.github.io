1ea4cfaa47e8740d8d5a32a8ee8a584baedf7e58
JohanM <johanm@centiro.se>
2024-09-24 16:56:52 +0200

    use AI

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import {SocialsComponent} from "../socials/socials.component";
 import {ProjectsComponent} from "../projects/projects.component";
 import {PublicationsComponent} from "../publications/publications.component";
 import { TimeLineComponent } from '../time-line/time-line.component';
+import {NgOptimizedImage} from "@angular/common";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimeLineComponent, SocialsComponent, ProjectsComponent, PublicationsComponent],
+  imports: [TimeLineComponent, SocialsComponent, ProjectsComponent, PublicationsComponent, NgOptimizedImage],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
