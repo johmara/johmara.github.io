f37e6faf1aab7cc1ae281737aab04260183f1632
JohanM <johanm@centiro.se>
2024-09-30 09:35:04 +0200

    remove unused routes and usehashroutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {PublicationsListComponent} from "./publications-list/publications-list.component";
-import {TimeLineComponent} from "./time-line/time-line.component";
 
 export const routes: Routes = [
-  {path: '', pathMatch: 'full', redirectTo:'home'},
-  {path: 'home', component: HomeComponent},
-  {path: 'resume', component: TimeLineComponent},
+  {path: '', pathMatch: 'full', component: HomeComponent},
   {path: 'publications', component: PublicationsListComponent},
 ];
