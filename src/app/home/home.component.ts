11cd7a5977847d641f32a91a1f26c4099d61cc85
johmara <johan.t.martinson@gmail.com>
2026-08-20 12:51:31 +0200

    ...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,8 @@
 import {Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 import {SocialsComponent} from "../socials/socials.component";
 import {ProjectsComponent} from "../projects/projects.component";
 import {PublicationsComponent} from "../publications/publications.component";
-import {TimeLineComponent} from '../time-line/time-line.component';
 import {NgOptimizedImage} from "@angular/common";
 import {PublicationsService} from '../publications.service';
 
@@ -10,7 +10,7 @@ import {PublicationsService} from '../publications.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [TimeLineComponent, SocialsComponent, ProjectsComponent, PublicationsComponent, NgOptimizedImage],
+  imports: [RouterLink, SocialsComponent, ProjectsComponent, PublicationsComponent, NgOptimizedImage],
   providers: [PublicationsService],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
